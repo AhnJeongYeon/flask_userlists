93ed7e93edb9bb15e3218e65dea7348274a3382a
Your Name <you@example.com>
2021-05-31 16:26:18 +0900

    hash256 step1

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, redirect, request
 from data import Articles
+import pymysql
+from passlib.hash import sha256_crypt
+
+db = pymysql.connect(
+            host='localhost', 
+            user='root', 
+            password='1234', 
+            db='gangnam',
+            charset='utf8mb4')
+
+cur = db.cursor()
+
 
 app = Flask(__name__)
 app.debug = True
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    password_1 = sha256_crypt.encrypt("1234")
+    print(sha256_crypt.verify("1234", password_1))
+    print(name, email, password)
+    return "SUCCESS"
 
 
 @app.route('/', methods=['GET', 'POST']) # 경로(그 쪽 경로에 입장하면 실행)/로그인,관리자OX
@@ -17,28 +39,123 @@ def about():
 
 @app.route('/articles', methods=['GET', 'POST'])
 def articles():
-    articles = Articles()
+    # articles = Articles()
+
+    query = 'SELECT * FROM topic;'
+
+    cur.execute(query)
+
+    db.commit()
+
+    articles = cur.fetchall()
+
+    print(articles)
     return render_template( "articles.html", articles = articles )
+
 
 @app.route('/article/<id>', methods=['GET', 'POST'])
 def article(id):
-    articles = Articles()
-    print(len(articles))
-    article = articles[int(id)-1]
-    if len(articles)<=int(id):
+    # articles = Articles()
+    # print(len(articles))
+    # article = articles[int(id)-1]
+    # if len(articles)<=int(id):
         
-        return render_template("article.html", article = article)
-    else:
-        return render_template("article.html", article = "NO DATA")
+    #     return render_template("article.html", article = article)
+    # else:
+    #     return render_template("article.html", article = "NO DATA")
 
-@app.route('/add_article', methods=['GET', 'POST'])
-def add_article():
-    return render_template( "add_article.html" )
+    query = f"SELECT * FROM topic WHERE id = {id} "
+
+    cur.execute(query)
+    db.commit()
+
+    article = cur.fetchall()
+    print(article)
+    if article **None:
+        return redirect('/articles')
+    else:
+        return render_template('article.html', article = article )
+
+
+@app.route('/article/<id>/delete')
+def delete_article(id):
+    query = f'DELETE FROM `topic` WHERE id = {id};'
+    cur.execute(query)
+
+    db.commit()
+
+    # db.close()
+
+    return redirect('/articles')
+
+@app.route('/add_article',  methods = ['GET','POST'])
+def add_articles():
+
+    if request.method == "POST" :
+
+        title = request.form['title']
+        description = request.form['description']
+        author = request.form['author']
+        
+        print(title, description, author)
+        # return "SUCCESS"
+
+        query = "INSERT INTO `topic` (`title`,`description`,`author`) VALUES (%s, %s, %s);"
+        input_data = [title, description ,author]
+        # print(request, description, author)
+
+        cur.execute(query,input_data)
+        db.commit()
+        print(cur.rowcount)
+        # db.close()
+
+        return redirect("/articles")
+        
+    else :
+        return render_template("add_article.html")
+
+@app.route("/article/<id>/edit", methods = ['GET','POST'])
+
+def edit_article(id):
+    if request.method == 'POST':
+
+        title = request.form['title']
+        description = request.form['description']
+
+        author = request.form['author']
+        # print(title, description, author)
+
+        # return "SUCCESS"
+        
+        query = f"UPDATE `gangnam`.`topic` SET  title='{title}',description= '{description}' ,author='{author}' WHERE id = {id};"
+        print(query)
+    
+        cur.execute(query)
+
+        db.commit()
+
+        return redirect('/articles')
+        
+
+    else:
+        query = f"SELECT * FROM topic WHERE id = {id};"
+
+
+        cur.execute(query)
+        db.commit()
+
+        article = cur.fetchone()
+
+        return render_template('edit_article.html', article = article)
+        
+
+        
 
 if __name__ == '__main__':
     app.run(port=5000)
 
-# 서버 띄우기 GET 방식으로 Enter만 치는것.
+# 서버 띄우기 GET 방식으로 Enter만 치는 것.
+
 
 
 
--- database_sql.py
+++ database_sql.py
@@ -33,13 +33,13 @@ cur = db.cursor()
 
 
 
-query = 'INSERT INTO `gangnam`.`topic` (`id`, `title`, `description`, `author`) VALUES (2 ,"자바" ,"자바(영어: Java, 문화어: 쟈바)는 썬 마이크로시스템즈의 제임스 고슬링(James Gosling)과 다른 연구원들이 개발한 객체 지향적 프로그래밍 언어이다. 1991년 그린 프로젝트(Green Project)라는 이름으로 시작해 1995년에 발표했다.", "GARY");'
+# query = 'INSERT INTO `gangnam`.`topic` (`id`, `title`, `description`, `author`) VALUES (2 ,"자바" ,"자바(영어: Java, 문화어: 쟈바)는 썬 마이크로시스템즈의 제임스 고슬링(James Gosling)과 다른 연구원들이 개발한 객체 지향적 프로그래밍 언어이다. 1991년 그린 프로젝트(Green Project)라는 이름으로 시작해 1995년에 발표했다.", "GARY");'
 
-cur.execute(query)
+# cur.execute(query)
 
-db.commit()
+# db.commit()
 
-db.close()
+# db.close()
 
 
 # query = "UPDATE `topic` SET `title` = 'asdfasf', `author` = 'asdfasfsa' WHERE (`id` = '2')" # WHERE : 조건문
@@ -56,4 +56,29 @@ db.close()
 
 # db.close()
 
+# query = "ALTER TABLE `gangnam`.`topic` CHANGE COLUMN `create_at` `create_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ;"
 
+# cur.execute(query)
+
+# db.commit()
+
+# db.close()
+
+
+
+query = ''' 
+        CREATE TABLE users(
+            id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+            name VARCHAR(100),
+            email VARCHAR(100),
+            username VARCHAR(30),
+            password VARCHAR(100),
+            register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+            ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ''' 
+
+cur.execute(query)
+
+db.commit()
+
+db.close()
